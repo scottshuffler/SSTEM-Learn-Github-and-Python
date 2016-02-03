28811845ee41d0e5b56146227e8913dcc997688f
Jonathan Brotherton <brothertonjd@appstate.edu>
2016-02-03 11:34:14 -0500

    Wrote my calculator and edited main method

--- main.py
+++ main.py
@@ -30,14 +30,43 @@ class Jonathan:
     def __init__(self):
         self.user_string = ''
 
-    def loop(self):
+    def add(self, uno, dos):
+        addop = uno + dos
+        return addop
+
+    def subtract(self, num1, num2):
+        subop = num1 - num2
+        return subop
+    def multiply(self, num1, num2):
+        multop = num1 * num2
+        return multop
+    def divide(self, num1, num2):
+        divop = num1 / num2
+        return divop
+    def main(self):
+        print('ezpz calculatorino')
         while True:
-            arg = input('-> ')
-            if arg == 'quit':
-                print(self.user_string)
+            num1 = int(input('Enter your first number: '))
+            op = input('Choose an operator(+,-,*,/)')
+            num2 = int(input('Enter your second number: '))
+            if (op == '+'):
+                answer = Jonathan().add( num1, num2)
+                print(num1, "+", num2, "=", answer)
+                break
+            elif (op == '-'):
+                answer = Jonathan().subtract(num1, num2)
+                print(num1, "-", num2, "=", answer)
+                break
+            elif (op == '*'):
+                answer = Jonathan().multiply(num1, num2)
+                print(num1, "*", num2, "=", answer)
+                break
+            elif (op == '/'):
+                answer = Jonathan().divide(num1, num2)
+                print(num1, "/", num2, "=", answer)
                 break
             else:
-                self.user_string += arg
+                print('invalid string') 
 
 
 if __name__ == '__main__':
@@ -48,4 +77,4 @@ if __name__ == '__main__':
     d.loop()
 
     j = Jonathan()
-    j.loop()
+    j.main()
